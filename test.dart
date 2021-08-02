c2b02a1f5f581e82ba2e78dfe6eb4a73160bd189
baselallam <baseljahen@gmail.com>
2021-08-02 18:52:43 +0200

    'commit'

---
+++ test.dart
@@ -0,0 +1,29 @@
+// Data Types
+    // String => represnt any character UTF-16
+    // Num
+      // int
+      // double
+    // bool
+    // list 
+    // set
+    // Map
+
+
+void main() {
+
+  String name = 'ahmed';
+
+  String email = 'ahmed.com';
+
+  String age = '20';
+
+  bool isMarried = false;
+
+  double height = 200.5 + double.parse(age);
+
+  print('Name is $name , Age is $age, email is $email, height is ' + height.toString());
+
+}
+
+
+// 
