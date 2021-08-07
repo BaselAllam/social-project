4db68e9a68e2d2779b24b5eb097d18d012bc2567
baselallam <baseljahen@gmail.com>
2021-08-07 17:35:49 +0200

    'commit'

--- test.dart
+++ test.dart
@@ -1,64 +1,67 @@
 
-// void main() {
-
-//   Map<int, Map<String, dynamic>> std = {
-//     1 : {
-//       'name' : 'ahmed',
-//       'email' : 'ahmed.com',
-//       'age' : 20,
-//       'friends' : [
-//         'ahmed',
-//         'mahmoud'
-//       ],
-//     },
-//     2 : {
-//       'name' : 'ali',
-//       'email' : 'ali.com',
-//       'age' : 30,
-//       'friends' : [
-//         'ahmed',
-//       ],
-//     },
-//     3 : {
-//       'name' : 'sayed',
-//       'email' : 'sayed.com',
-//       'age' : 210,
-//       'friends' : [
-//         'ahmed',
-//         'mahmoud',
-//         'mahmoud'
-//       ],
-//     },
-//   };
-
-  
-
-// }
-
-
-
 void main() {
 
+  Map<int, Map<String, dynamic>> std = {
+    1 : {
+      'name' : 'ahmed',
+      'email' : 'ahmed.com',
+      'age' : 20,
+      'friends' : [
+        'ahmed',
+        'mahmoud'
+      ],
+      'familyMember' : [
+        {
+          'name' : 'ahmed father',
+          'age' : 50
+        },
+        {
+          'name' : 'ahmed mother',
+          'age' : 40
+        },
+        {
+          'name' : 'ahmed brother',
+          'age' : 10
+        }
+      ]
+    },
+    2 : {
+      'name' : 'ali',
+      'email' : 'ali.com',
+      'age' : 30,
+      'friends' : [
+        'ahmed',
+      ],
+    },
+    3 : {
+      'name' : 'sayed',
+      'email' : 'sayed.com',
+      'age' : 210,
+      'friends' : [
+        'ahmed',
+        'mahmoud',
+        'mahmoud'
+      ],
+      'familyMember' : [
+        {
+          'name' : 'sayed father',
+          'age' : 60
+        },
+        {
+          'name' : 'sayed mother',
+          'age' : 50
+        },
+        {
+          'name' : 'sayed brother',
+          'age' : 15
+        }
+      ]
+    },
+  };
 
-  int x = 10;
-
-  int y = 20;
-
-  // () => header 
-
-  // {} => body
-
-  if(x != y) {
-    print('its ==');
-    if(x < y) {
-      print('im nesterd');
-    }else{
-      print('its >');
-    }
-  }else if(x > y) {
-    print('its >');
-  }else{
-    print('its !=');
-  }
+  print('''
+  ${std[1]!['familyMember'][0]['name']},
+  ${std[1]!['familyMember'][1]['name']}
+  ''');
 
 }
