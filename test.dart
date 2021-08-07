37cf216a255866a4072c63930c979a00024fdda2
baselallam <baseljahen@gmail.com>
2021-08-07 18:33:12 +0200

    'commit'

--- test.dart
+++ test.dart
@@ -1,67 +1,94 @@
 
-void main() {
+// void main() {
 
-  Map<int, Map<String, dynamic>> std = {
-    1 : {
-      'name' : 'ahmed',
-      'email' : 'ahmed.com',
-      'age' : 20,
-      'friends' : [
-        'ahmed',
-        'mahmoud'
-      ],
-      'familyMember' : [
-        {
-          'name' : 'ahmed father',
-          'age' : 50
-        },
-        {
-          'name' : 'ahmed mother',
-          'age' : 40
-        },
-        {
-          'name' : 'ahmed brother',
-          'age' : 10
-        }
-      ]
-    },
-    2 : {
-      'name' : 'ali',
-      'email' : 'ali.com',
-      'age' : 30,
-      'friends' : [
-        'ahmed',
-      ],
-    },
-    3 : {
-      'name' : 'sayed',
-      'email' : 'sayed.com',
-      'age' : 210,
-      'friends' : [
-        'ahmed',
-        'mahmoud',
-        'mahmoud'
-      ],
-      'familyMember' : [
-        {
-          'name' : 'sayed father',
-          'age' : 60
-        },
-        {
-          'name' : 'sayed mother',
-          'age' : 50
-        },
-        {
-          'name' : 'sayed brother',
-          'age' : 15
-        }
-      ]
-    },
-  };
+//   Map<int, Map<String, dynamic>> std = {
+//     1 : {
+//       'name' : 'ahmed',
+//       'email' : 'ahmed.com',
+//       'age' : 20,
+//       'friends' : [
+//         'ahmed',
+//         'mahmoud'
+//       ],
+//       'familyMember' : [
+//         {
+//           'name' : 'ahmed father',
+//           'age' : 50
+//         },
+//         {
+//           'name' : 'ahmed mother',
+//           'age' : 40
+//         },
+//         {
+//           'name' : 'ahmed brother',
+//           'age' : 10
+//         }
+//       ]
+//     },
+//     3 : {
+//       'name' : 'sayed',
+//       'email' : 'sayed.com',
+//       'age' : 210,
+//       'friends' : [
+//         'ahmed',
+//         'mahmoud',
+//         'mahmoud'
+//       ],
+//       'familyMember' : [
+//         {
+//           'name' : 'sayed father',
+//           'age' : 60
+//         },
+//         {
+//           'name' : 'sayed mother',
+//           'age' : 50
+//         },
+//         {
+//           'name' : 'sayed brother',
+//           'age' : 15
+//         }
+//       ]
+//     },
+//   };
 
-  print('''
-  ${std[1]!['familyMember'][0]['name']},
-  ${std[1]!['familyMember'][1]['name']}
-  ''');
+//   std.forEach((key, value) {
+//     List o = [];
+//     value['familyMember'].forEach((i) {
+//       o.add(i['age']);
+//     });
+//     num sum = o.reduce((value, element) => value + element);
+//     print(sum);
+//   });
 
-}
+// }
+
+
+
+List<Map<String, dynamic>> data = [
+  {
+    'name' : 'std1',
+    'scores' : {
+      'math' : 200,
+      'science' : 300,
+    },
+    'success' : false
+  },
+  {
+    'name' : 'std12',
+    'scores' : {
+      'math' : 100,
+      'science' : 100,
+    },
+    'success' : true
+  },
+  {
+    'name' : 'std1',
+    'scores' : {
+      'math' : 250,
+      'science' : 350,
+    },
+    'success' : true
+  },
+];
+
+// sucess > 500
