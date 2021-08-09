83750244b47757f60a8863b348ec70176c2e58ce
baselallam <baseljahen@gmail.com>
2021-08-09 18:18:00 +0200

    'commit'

---
+++ demodata.dart
@@ -0,0 +1,86 @@
+
+ Map<int, Map<String, dynamic>> std = {
+    1 : {
+      'name' : 'ahmed',
+      'email' : 'ahmed.com',
+      'age' : 20,
+      'friends' : [
+        'ahmed',
+        'mahmoud'
+      ],
+      'familyMember' : [
+        {
+          'name' : 'ahmed father',
+          'age' : 50
+        },
+        {
+          'name' : 'ahmed mother',
+          'age' : 40
+        },
+        {
+          'name' : 'ahmed brother',
+          'age' : 10
+        }
+      ]
+    },
+    3 : {
+      'name' : 'sayed',
+      'email' : 'sayed.com',
+      'age' : 210,
+      'friends' : [
+        'ahmed',
+        'mahmoud',
+        'mahmoud'
+      ],
+      'familyMember' : [
+        {
+          'name' : 'sayed father',
+          'age' : 60
+        },
+        {
+          'name' : 'sayed mother',
+          'age' : 50
+        },
+        {
+          'name' : 'sayed brother',
+          'age' : 15
+        }
+      ]
+    },
+  };
+
+
+List<Map<String, dynamic>> data = [
+  {
+    'name' : 'std1',
+    'scores' : {
+      'math' : 200,
+      'science' : 300,
+    },
+    'success' : false
+  },
+  {
+    'name' : 'std12',
+    'scores' : {
+      'math' : 100,
+      'science' : 100,
+    },
+    'success' : true
+  },
+  {
+    'name' : 'std3',
+    'scores' : {
+      'math' : 250,
+      'science' : 350,
+    },
+    'success' : true
+  },
+  {
+    'name' : 'std4',
+    'scores' : {
+      'math' : 150,
+      'science' : 50,
+    },
+    'success' : true
+  },
+];
--- test.dart
+++ test.dart
@@ -1,101 +1,56 @@
+import 'demodata.dart';
 
 // void main() {
 
-//   Map<int, Map<String, dynamic>> std = {
-//     1 : {
-//       'name' : 'ahmed',
-//       'email' : 'ahmed.com',
-//       'age' : 20,
-//       'friends' : [
-//         'ahmed',
-//         'mahmoud'
-//       ],
-//       'familyMember' : [
-//         {
-//           'name' : 'ahmed father',
-//           'age' : 50
-//         },
-//         {
-//           'name' : 'ahmed mother',
-//           'age' : 40
-//         },
-//         {
-//           'name' : 'ahmed brother',
-//           'age' : 10
-//         }
-//       ]
-//     },
-//     3 : {
-//       'name' : 'sayed',
-//       'email' : 'sayed.com',
-//       'age' : 210,
-//       'friends' : [
-//         'ahmed',
-//         'mahmoud',
-//         'mahmoud'
-//       ],
-//       'familyMember' : [
-//         {
-//           'name' : 'sayed father',
-//           'age' : 60
-//         },
-//         {
-//           'name' : 'sayed mother',
-//           'age' : 50
-//         },
-//         {
-//           'name' : 'sayed brother',
-//           'age' : 15
-//         }
-//       ]
-//     },
-//   };
-
-//   std.forEach((key, value) {
-//     List o = [];
-//     value['familyMember'].forEach((i) {
-//       o.add(i['age']);
+//   for(int i = 0; i < data.length; i++) {
+//     num sum = 0;
+//     data[i]['scores'].forEach((k, v) {
+//       sum += v;
 //     });
-//     num sum = o.reduce((value, element) => value + element);
-//     print(sum);
-//   });
+//     if(sum < 250) {
+//       data.removeAt(i);
+//     }else{
+//       stdScoreValidator(sum, data[i]);
+//     }
+//   }
+
+//   print(data);
 
 // }
 
 
+stdScoreValidator(num sum, Map<String, dynamic> std) {
 
-List<Map<String, dynamic>> data = [
-  {
-    'name' : 'std1',
-    'scores' : {
-      'math' : 200,
-      'science' : 300,
-    },
-    'success' : false
-  },
-  {
-    'name' : 'std12',
-    'scores' : {
-      'math' : 100,
-      'science' : 100,
-    },
-    'success' : true
-  },
-  {
-    'name' : 'std3',
-    'scores' : {
-      'math' : 250,
-      'science' : 350,
-    },
-    'success' : true
-  },
-  {
-    'name' : 'std4',
-    'scores' : {
-      'math' : 150,
-      'science' : 50,
-    },
-    'success' : true
-  },
-];
+  if(sum < 500) {
+    std['success'] = false;
+  }else{
+    std['success'] = true;
+  }
+}
 
+main() {
+
+  for(int i = 0; i < data.length; i++) {
+    num sum = 0;
+    data[i]['scores'].forEach((k, v) {
+      sum += v;
+    });
+    if(sum < 250) {
+      data.removeAt(i);
+      i--;
+    }else{
+      stdScoreValidator(sum, data[i]);
+      studentData(data[i]);
+    }
+  }
+}
+
+
+studentData(Map<String, dynamic> std) {
+
+  print('''
+  Std Name: ${std['name']},
+  Std Scorse ${std['scores']},
+  Std Stuts ${std['success']}
+  ''');
+}
