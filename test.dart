a3779c6c7a37a867b28f03e79d17a8ac3590ced3
baselallam <baseljahen@gmail.com>
2021-08-16 18:53:45 +0200

    'commit'

--- test.dart
+++ test.dart
@@ -1,56 +1,29 @@
-import 'demodata.dart';
 
-// void main() {
-
-//   for(int i = 0; i < data.length; i++) {
-//     num sum = 0;
-//     data[i]['scores'].forEach((k, v) {
-//       sum += v;
-//     });
-//     if(sum < 250) {
-//       data.removeAt(i);
-//     }else{
-//       stdScoreValidator(sum, data[i]);
-//     }
-//   }
-
-//   print(data);
-
-// }
+// Models
+  // User
+    // User Model
+    // User Controller
+  // Post
+    // Post Model
+    // Post Controller
+  // Pages
+    // Pages Model
+    // Pages Controller
 
 
-stdScoreValidator(num sum, Map<String, dynamic> std) {
+// -------------- //
 
-  if(sum < 500) {
-    std['success'] = false;
-  }else{
-    std['success'] = true;
-  }
-}
-
-main() {
-
-  for(int i = 0; i < data.length; i++) {
-    num sum = 0;
-    data[i]['scores'].forEach((k, v) {
-      sum += v;
-    });
-    if(sum < 250) {
-      data.removeAt(i);
-      i--;
-    }else{
-      stdScoreValidator(sum, data[i]);
-      studentData(data[i]);
-    }
-  }
-}
+// OOP:
+  // class
+  // object
+  // constructor & named constructor
+  // class methods
+  // Access Modifier
+  // Inheritence
+  // Multiple Inheritence
 
 
-studentData(Map<String, dynamic> std) {
-
-  print('''
-  Std Name: ${std['name']},
-  Std Scorse ${std['scores']},
-  Std Stuts ${std['success']}
-  ''');
-}
+// Read about Design Pattern
+// Read about OOP
+// Difference Between ( Object, Instance )
+// Prototype for Project
