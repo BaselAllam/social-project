27a83e7355aa6595b63f06e28bd411d147087d13
baselallam <baseljahen@gmail.com>
2021-08-18 18:56:49 +0200

    'commit'

--- demodata.dart
+++ demodata.dart
@@ -21,3 +21,55 @@
       'age' : 40,
     },
   };
+
+  
+
+   Map<int, Map<String, dynamic>> std = {
+    1 : {
+      'name' : 'ahmed',
+      'email' : 'ahmed.com',
+      'age' : 20,
+      'friends' : [
+        'ahmed',
+        'mahmoud'
+      ],
+      'familyMember' : [
+        {
+          'name' : 'ahmed father',
+          'age' : 50
+        },
+        {
+          'name' : 'ahmed mother',
+          'age' : 40
+        },
+        {
+          'name' : 'ahmed brother',
+          'age' : 10
+        }
+      ]
+    },
+    3 : {
+      'name' : 'sayed',
+      'email' : 'sayed.com',
+      'age' : 210,
+      'friends' : [
+        'ahmed',
+        'mahmoud',
+        'mahmoud'
+      ],
+      'familyMember' : [
+        {
+          'name' : 'sayed father',
+          'age' : 60
+        },
+        {
+          'name' : 'sayed mother',
+          'age' : 50
+        },
+        {
+          'name' : 'sayed brother',
+          'age' : 15
+        }
+      ]
+    },
+  };
