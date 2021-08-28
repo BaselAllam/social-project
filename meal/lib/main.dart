f5526397db11031a3376669de1ce1461beb4cdc2
baselallam <baseljahen@gmail.com>
2021-08-28 18:22:18 +0200

    'commit'

---
+++ meal/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
